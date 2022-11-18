d2f32c9895da7ea8f5e755d729bfa2d580944350
Blackman99 <1197160272@qq.com>
2022-11-18 10:36:24 +0800

    docs: update README

--- README.md
+++ README.md
@@ -2,6 +2,26 @@
 
 The Svelte version of Casual UI
 
+## Install
+
+```sh
+npm install @casual-ui/svelte
+```
+
+## Usage
+
+* Import styles in your client entry
+```js
+import '@casual-ui/dist/style.css'
+```
+* Use in .svelte files
+```html
+<script>
+  import { CButton } from '@casual-ui/svelte'
+</script>
+<CButton label="A Button" />
+```
+
 ## LICENSE
 
 [MIT](https://opensource.org/licenses/MIT)
--- packages/docs/svelte.config.js
+++ packages/docs/svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	},
-	package: {
-		dir: 'dist'
-	}
-};
+  kit: {
+    adapter: adapter(),
+  },
+  package: {
+    dir: 'dist',
+  },
+}
 
-export default config;
+export default config
