602448cdfe12311bf0a7afc8588effd802423e64
Blackman99 <1197160272@qq.com>
2022-12-21 15:22:10 +0800

    fix: use dynamic import to import demos

--- packages/docs/src/routes/components/CodeDemo.svelte
+++ packages/docs/src/routes/components/CodeDemo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
   import { CExpansion } from '@casual-ui/svelte'
+  import type { SvelteComponent } from 'svelte'
   import { page } from '$app/stores'
 
   export let demo: {
@@ -10,31 +10,41 @@
     slug: string
   }
 
-  onMount(() => {
-    const id = demo.slug
-    const link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.href = `/demos${$page.route.id}/demos/${id}/style.css`
-    document.head.append(link)
-  
-    const script = document.createElement('script')
-    script.src = `/demos${$page.route.id}/demos/${id}/main.js`
-    document.body.append(script)
+  const componentName = $page.route.id?.split('/').pop()
 
-    return () => {
-      script.remove()
-      link.remove()
-    }
-  })
+  let DemoComponent: any
+  import(`./${componentName}/demos/${demo.slug}/doc.svelte`).then((module) => {
+    DemoComponent = module.default
+})
+
+  // onMount(() => {
+  //   const id = demo.slug
+
+
+  //   const link = document.createElement('link')
+  //   link.rel = 'stylesheet'
+  //   link.href = `/demos${$page.route.id}/demos/${id}/style.css`
+  //   document.head.append(link)
+  
+  //   const script = document.createElement('script')
+  //   script.src = `/demos${$page.route.id}/demos/${id}/main.js`
+  //   document.body.append(script)
+
+  //   return () => {
+  //     script.remove()
+  //     link.remove()
+  //   }
+  // })
 </script>
 
 <div bg-white dark:bg-111111 mb-8 shadow-sm rounded-md>
   <div text-lg font-bold leading-16 pl-4>
     {demo.name}
   </div>
-  <div p-4>
-    <div id={demo.slug}></div>
-    <!-- <iframe on:load={computedHeight} bind:this={iframe} title={demo.name} src={demo.iframeUrl} class="sandbox" style={`height: ${contentHeight}`}></iframe> -->
+  <div p-4 id={demo.slug}>
+    {#if DemoComponent}
+      <svelte:component this={DemoComponent} />
+    {/if}
   </div>
   <CExpansion reverse title="Fold/Expand code"  headerStyle="position: sticky; bottom: 0;font-size: 14px;">
     <div i-vscode-icons-file-type-svelte slot="icon"></div>
