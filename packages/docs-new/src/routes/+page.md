a00980e7512452e192bdd0fe3dff4fd87910dc43
Blackman99 <1197160272@qq.com>
2023-02-20 11:20:06 +0800

    docs: initial docs new

---
+++ packages/docs-new/src/routes/+page.md
@@ -0,0 +1,17 @@
+---
+title: An UI components lib for Svelte3+
+heroImage: /logo@3x.png
+tagline: A simple, easy to use UI components lib for Svelte developers.
+actions:
+  - label: View on github
+    type: primary
+    to: https://github.com/Casual-UI/svelte
+    external: true
+features:
+  - title: Fully typed
+    description: Written with typescript. Strong type tips.
+  - title: Build with Sveltekit
+    description: Preserve the full power of Sveltekit. So that you can do more than SSG
+  - title: Svelte in Markdown
+    description: Feel free to use svelte in markdown. Explore infinite possibilities.
+---
---
+++ packages/docs-new/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import { sveltepress } from '@sveltepress/vite'
+import { defaultTheme } from '@sveltepress/theme-default'
+
+const config = defineConfig({
+  plugins: [
+    sveltepress({
+      theme: defaultTheme({
+        navbar: [
+          // Add your navbar configs here
+        ],
+        sidebar: {
+          // Add your sidebar configs here
+        },
+        github: 'https://github.com/Casual-UI/casual-ui',
+        logo: '/logo.png',
+        themeColor: {
+          light: '#fff',
+          dark: '#000',
+        },
+      }),
+      siteConfig: {
+        title: 'Casual UI - Svelte',
+        description: 'A component library that supports Svelte3+',
+      },
+    }) as any,
+  ],
+})
+
+export default config
