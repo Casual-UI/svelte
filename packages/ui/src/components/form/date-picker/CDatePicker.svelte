1fb66d18b127b40dae547009cb1580538d5dc197
Dongsheng <1197160272@qq.com>
2023-06-24 16:16:04 +0800

    fix: dayjs export

--- packages/ui/src/components/form/date-picker/CDatePicker.svelte
+++ packages/ui/src/components/form/date-picker/CDatePicker.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { dayjs } from 'dayjs/esm'
+  import * as dayjs from 'dayjs/esm'
   import { getContext } from 'svelte'
   import createClickOutsideAction from '../../../actions/createClickOutsideAction'
   import { validateCurrentKey } from '../../../hooks/useForm'
